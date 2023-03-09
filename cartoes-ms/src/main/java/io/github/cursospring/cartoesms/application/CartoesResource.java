7f59147510d07ec695b05229f607bcdfd72b0c0f
João Oliveira <joaoholiveira1@hotmail.com>
2023-03-09 18:06:38 -0300

    Listando os cartões de acordo com a renda.

--- cartoes-ms/src/main/java/io/github/cursospring/cartoesms/application/CartoesResource.java
+++ cartoes-ms/src/main/java/io/github/cursospring/cartoesms/application/CartoesResource.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("cartoes")
 @RequiredArgsConstructor
@@ -27,5 +29,10 @@ public class CartoesResource {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesRendaAte(@RequestParam("renda") Long renda){
+        List<Cartao> cartoesRendaMaiorOuIgual = service.getCartoesRendaMaiorOuIgual(renda);
+        return ResponseEntity.ok(cartoesRendaMaiorOuIgual);
+    }
 
 }
